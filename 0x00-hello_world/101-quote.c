008f8328b29c725afbcec992062bf72e98d4fe73
shinali02 <felixshinali2001@gmail.com>
2023-07-14 21:21:26 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unitstd.h>
+#include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
  * Return 1 (Success)
@@ -7,5 +7,5 @@
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+return (1);
 }
